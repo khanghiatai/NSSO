4ed40d934b44a1de24eafc9473753c866caaaec8
khanghiatai <khanghiatai@gmail.com>
2017-11-29 18:17:44 +0700

    move eclipse to intelliji

---
+++ src/test/java/modules/LoginTest.java
@@ -0,0 +1,151 @@
+package modules;
+
+import java.net.MalformedURLException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import configruration.ResourceHasMap;
+import libraries.LoginFunctions;
+import supports.CommonFunctions;
+
+public class LoginTest {
+
+	public static WebDriver driver; 	
+	ResourceHasMap resource = new ResourceHasMap();
+	LoginFunctions login;
+	String strUrl;
+	
+	@Test
+	private void login001_CheckUrl() {
+		strUrl = driver.getCurrentUrl();
+		strUrl = CommonFunctions.chuyenDoiKyTu(strUrl, "dang-nhap", "");
+		driver.navigate().to(strUrl + resource.getResource("urlaccount"));
+		Assert.assertEquals(driver.getTitle(), "Login");
+		driver.navigate().to(strUrl + resource.getResource("urlchangepass"));
+		Assert.assertEquals(driver.getTitle(), "Login");
+		driver.navigate().to(strUrl + resource.getResource("urlupdateavatar"));
+		Assert.assertEquals(driver.getTitle(), "Login");
+		driver.navigate().to(strUrl + resource.getResource("urlsession"));
+		Assert.assertEquals(driver.getTitle(), "Login");
+		driver.navigate().to(strUrl + resource.getResource("urladress"));
+		Assert.assertEquals(driver.getTitle(), "Login");
+	}	
+	
+	@Test(dataProvider = "listUserAccount")
+	private void login002_Login(String email, String password) {		
+		login.login(driver, email, password); 		
+		if(email == " " && password != " ")
+			login.checkEmailNull(driver, email, password);
+		if(email != " " && password == " ")
+			login.checkPassNull(driver, email, password);
+		if(email != " " && password != " ")
+			login.checkWrongAccount(driver, email, password);			 
+	}
+	
+	@Test(dataProvider = "listUserInfo")
+	private void login003_UpdateInfo(String fName, String lName, String gender, String day, String month, String year, String text) {
+		String time = new SimpleDateFormat("yyMMdd_HHmmss").format(Calendar.getInstance().getTime());getClass();
+
+		String strTitle = driver.getTitle();
+		if(strTitle.equalsIgnoreCase(resource.getResource("infobasic"))) {
+			login.updateUserInfo(driver, fName + time, lName + time,  gender, day, month, year, text  + time);			
+		} else { // link from other page
+			driver.navigate().to(strUrl + resource.getResource("urlaccount"));
+			login.updateUserInfo(driver, fName + time, lName + time,  gender, day, month, year, text + time);
+		}
+	}
+	
+	@Test(dataProvider = "listChangePass")
+	private void login004_ChangePassword(String oldPass, String newPass, String confirmPass) {
+		login.changePassword(driver, oldPass, newPass, confirmPass);
+		if(newPass != confirmPass) {
+			login.checkMessageNull(driver, confirmPass, "ConfirmNewPassword-error", resource.getResource("confirmpassnull"));
+		} else {
+			if(oldPass == "") {
+				login.checkMessageNull(driver, oldPass, "OldPassword-error", resource.getResource("oldpassnull"));
+			} if(newPass == "") {
+				login.checkMessageNull(driver, newPass, "NewPassword-error", resource.getResource("newpassnull"));
+			} if(confirmPass == "") {
+				login.checkMessageNull(driver, confirmPass, "ConfirmNewPassword-error", resource.getResource("confirmpassnull"));
+			}
+		}
+	}
+	
+	@Test
+	private void login010_Logout() {
+		login.logout(driver); 
+		login.clickForgotPassWord(driver); 
+	}
+	
+	@Test(dataProvider = "listEmail")
+	private void login011_ForgetPassword(String email) {		
+		login.forgotPassword(driver, email); 
+		login.checkMesageGetPass(driver, email);
+	}
+
+	@DataProvider
+	public Object[][] listUserAccount() {
+		return new Object[][] { 
+			new Object[] { " ", " " },
+			new Object[] { " ", "123123" }, 
+			new Object[] { "tai.kha", " " },
+			new Object[] { "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890", 
+					"123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890" }, 
+			//new Object[] { "!@#$%^&*()-=_+[]|;',./<>?", "!@#$%^&*()-=_+[]|;',./<>?" }, 
+			new Object[] { "khangnghiatai@gmail.com", "123456" }, 
+		};
+	}
+
+	@DataProvider
+	public Object[][] listUserInfo() {
+		return new Object[][] { 
+			new Object[] { "firstName 1", "lastName 1", resource.getResource("male"), "1", "1", "1990", "text area 1" },
+			new Object[] { "firstName 2", "lastName 2", resource.getResource("female"), "2", "2", "1992", "text area 2" },		
+		};
+	}
+	
+	@DataProvider
+	public Object[][] listEmail() {
+		return new Object[][] { 
+			new Object[] {""},
+			new Object[] {""},	
+			new Object[] {"123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"},
+			new Object[] {"tai23232@gmail.com"},
+			new Object[] {"khangnghiatai@gmail.com"},
+		};
+	}
+	
+	@DataProvider
+	public Object[][] listChangePass() {
+		return new Object[][] { 
+			new Object[] { "", "", "" },
+			new Object[] { "123456", "", "" },
+			new Object[] { "", "123123", "123456" },
+			new Object[] { "123456", "", "123123" },
+			new Object[] { "123456", "123123", "" },
+			new Object[] { "123456", "123123", "123123" },
+			new Object[] { "123123", "123456", "123456" },		
+		};
+	}
+	
+	@Parameters({ "BROWSER", "URL" })
+	@BeforeTest
+	public void beforeClass(String BROWSER, String URL) throws MalformedURLException {
+		driver = CommonFunctions.getBrowser(BROWSER);
+		CommonFunctions.visit(driver, URL);
+		login = new LoginFunctions(driver);
+		strUrl = URL;
+	}	
+	
+	@AfterTest
+	public void afterClass() {
+		driver.quit();
+	}
+}
---
+++ src/test/java/objects/LoginPage.java
@@ -0,0 +1,84 @@
+package objects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+//import org.openqa.selenium.supports.pagefactory.AjaxElementLocatorFactory;
+
+public class LoginPage {
+
+	public WebDriver driver; 
+	
+	public LoginPage(WebDriver driver) {
+		//AjaxElementLocatorFactory factory = new AjaxElementLocatorFactory(driver,100);
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(id = "Email")
+	public WebElement txt_Email;
+	
+	@FindBy(id = "Password")
+	public WebElement txt_Password;
+	
+	@FindBy(xpath = "//*[@id='bt_submit']")
+	public WebElement btn_Submit;
+	
+	@FindBy(id = "FirstName")
+	public WebElement txt_FirstName;
+	
+	@FindBy(id = "LastName")
+	public WebElement txt_LastName;
+	
+	@FindBy(id = "Description")
+	public WebElement txt_Description;
+	
+	@FindBy(id = "IsPrivate")
+	public WebElement chk_IsPrivate;
+	
+	@FindBy(id = "IsMale")
+	public WebElement ddl_IsMale;
+	
+	@FindBy(id = "DateOfBirthDay")
+	public WebElement ddl_DateOfBirthDay;
+	
+	@FindBy(id = "DateOfBirthMonth")
+	public WebElement ddl_DateOfBirthMonth;
+	
+	@FindBy(id = "DateOfBirthYear")
+	public WebElement ddl_DateOfBirthYear;
+	
+	@FindBy(xpath = ".//*[@data-valmsg-for='Email']")
+	public WebElement lbl_EmailNull;
+	
+	@FindBy(xpath = "//*[@id='Password-error']")
+	public WebElement lbl_PasswordNull;
+	
+	@FindBy(xpath = "//*[@class='bs-example-bg-classes']")
+	public WebElement lbl_UpdateSuccess;
+	
+	@FindBy(className = "profile-usertitle-name")
+	public WebElement lbl_UserTitleName;
+	
+	@FindBy(name = "OldPassword")
+	public WebElement txt_OldPass;
+	
+	@FindBy(name = "NewPassword")
+	public WebElement txt_NewPassword;
+	
+	@FindBy(name = "ConfirmNewPassword")
+	public WebElement txt_ConfirmNewPassword;
+	
+	@FindBy(id = "proChangePass")
+	public WebElement lnk_ChangePassword;
+	
+	@FindBy(id = "OldPassword-error")
+	public WebElement lbl_oldPassError;
+	
+	@FindBy(id = "NewPassword-error")
+	public WebElement lbl_NewPassError;
+	
+	@FindBy(xpath = ".//input[@id='RememberMe']/following-sibling::span/a")
+	public WebElement hpl_forgotPass;
+	
+}
